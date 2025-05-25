175dfd259aee74b0894d1e15f0b96e4c090f2b60
lim-jaein <limjaein00@gmail.com>
2025-05-25 15:42:07 +0900

    요청/응답 DTO 추가

--- src/main/java/io/github/im_jaein/duudu/controller/TodoController.java
+++ src/main/java/io/github/im_jaein/duudu/controller/TodoController.java
@@ -1,8 +1,11 @@
 package io.github.im_jaein.duudu.controller;
 
 import io.github.im_jaein.duudu.domain.entity.Todo;
+import io.github.im_jaein.duudu.dto.TodoRequestDto;
+import io.github.im_jaein.duudu.dto.TodoResponseDto;
 import io.github.im_jaein.duudu.service.TodoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,14 +19,14 @@ public class TodoController {
     private final TodoService todoService;
 
     @PostMapping
-    public ResponseEntity<Todo> createTodo(@RequestBody String content) {
-        Todo todo = todoService.createTodo(content);
-        return ResponseEntity.ok(todo);
+    public ResponseEntity<TodoResponseDto> createTodo(@RequestBody TodoRequestDto requestDto) {
+        TodoResponseDto todo = todoService.createTodo(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(todo);
     }
 
     @GetMapping
-    public ResponseEntity<List<Todo>> getAllTodos() {
-        List<Todo> todos = todoService.getAllTodos();
+    public ResponseEntity<List<TodoResponseDto>> getAllTodos() {
+        List<TodoResponseDto> todos = todoService.getAllTodos();
         return ResponseEntity.ok(todos);
     }
 }
